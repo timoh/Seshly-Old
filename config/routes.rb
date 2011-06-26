47cc858fd986bda00020479121e442155119f180
Joakim Runeberg <joakim.runeberg@gmail.com>
2011-06-27 02:59:15 +0300

    installed rspec for tests

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Seshly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "spots#index"
+  root :to => 'spots#index'
 
   # See how all your routes lay out with "rake routes"
 
