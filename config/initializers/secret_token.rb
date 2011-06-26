25539df3eafff4faa858a5b00e6743530b059550
Timo H <timo@dealmachine.net>
2011-06-26 18:13:35 +0300

    first commit, Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seshly::Application.config.secret_token = '84eab9dc62c4341bc65642c2ab176faf8c9a9cf030b3aed7ec4280463fdee280fdccf3c50aecff743625bb85f3b997c7ea2656588575a1743786b4e5f2906010'
